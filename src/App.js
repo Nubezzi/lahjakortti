84da156d08d955899c119316c1d4bb7737c09cc5
Nubezzi <patenur@hotmail.com>
2022-12-23 17:30:06 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { send } from 'emailjs-com';
+import kuva from './imgs/Untitled.png';
 
 function App() {
+
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: '',
+    message: '',
+    reply_to: '',
+  });
+
+  function Mailform() {
+    return(
+      <form onSubmit={onSubmit}>
+          <input
+            type='text'
+            name='from_name'
+            placeholder='Varaaja'
+            value={toSend.from_name}
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            name='date'
+            placeholder='Päivämäärä'
+            value={toSend.to_name}
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            name='message'
+            placeholder='Päivän toiveohjelma'
+            value={toSend.message}
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            name='info'
+            placeholder='Muuta tietoa?'
+            value={toSend.reply_to}
+            onChange={handleChange}
+          />
+          <button type='submit'>Varaa aika!</button>
+        </form>
+    )
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_lahjakortti',
+      'template_couuawj',
+      toSend,
+      'KXUUTcmjiSRG6n4HB'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <html>
+      <header>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet" />
       </header>
-    </div>
+      <body>
+        <div className='h1h1'>
+          <h1>Hyvää joulua</h1>
+          <h2>Tässä voit lunastaa lahjakortin halutulla toiveohjelmalla, valitsemallesi päivälle.</h2>
+          <h3>Eli siis lahjakortti ei ole mitään 750€, vaan tietenkin laatuperheaikaa! :)</h3>
+        </div>
+        <div className="main-block">
+          <div className="left-part">
+          </div>
+          <form action="/">
+            <h1>Varaa "Elämys"-lahja!</h1>
+            <div className="info">
+              <input type='text' name='from_name'placeholder='Varaaja' value={toSend.from_name} onChange={handleChange}/>
+              <input type='date'name='date' min="2023-01-15" value={toSend.to_name} onChange={handleChange}/>
+              <input type='text' name='info' placeholder='Muuta tietoa?' value={toSend.reply_to} onChange={handleChange}/>
+            </div>
+            <p>Päivän toiveohjelma</p>
+            <div>
+              <textarea type='text' name='message' value={toSend.message} onChange={handleChange} rows="4"></textarea>
+            </div>
+            <button type="submit" href="/">Varaa!</button>
+          </form>
+        </div>
+      </body>
+    </html>
   );
 }
 
